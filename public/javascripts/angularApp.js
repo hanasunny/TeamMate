c131f83e777034e7ea8b87d309d97df7fe45fd54
Pieter Bork <pcbork@gmail.com>
2016-03-03 22:50:46 -0700

    basic operations

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,7 +1,57 @@
-var app = angular.module('TeamMate', []);
+var app = angular.module('TeamMate', ['ui.router']);
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.green = 'greeeeeeeeeeen!';
+'teams',
+function($scope, teams){
+	$scope.teams = teams.teams;
+  $scope.addTeam = function() {
+  	if(!$scope.title || $scope.title === '') { return; }
+  	$scope.teams.push({title: $scope.title, members: 0})
+  	$scope.title = '';
+  }
+}]);
+
+app.controller('TeamsCtrl', [
+'$scope',
+'$stateParams',
+'teams',
+function($scope, $stateParams, teams){
+	$scope.team = teams.teams[$stateParams.id];
+	$scope.addMember = function() {
+		$scope.team.members.push({name: $scope.name})
+		$scope.name = '';
+	}
+}]);
+
+app.factory('teams', [function() {
+	var o = {
+		teams: [
+  	{title: 'Team1', members: [{name: 'Pieter'}, {name: 'Pieter2'}]},
+  	{title: 'Team2', members: [{name: 'Hana'},]},
+  	{title: 'Team3', members: [{name: 'Cody'},]},
+  	{title: 'Team4', members: [{name: 'Tiffany'},]}
+		]
+	};
+	return o;
+}])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+		.state('teams', {
+		  url: '/teams/{id}',
+		  templateUrl: '/teams.html',
+		  controller: 'TeamsCtrl'
+		});
+
+  $urlRouterProvider.otherwise('home');
 }]);
